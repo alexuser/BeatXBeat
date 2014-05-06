81c805e241bbe889bfc465325294a7033a722894
Alex Chen <chen.alex@berkeley.edu>
2014-05-06 02:28:51 -0700

    Share intent for transcribe function
    Convert buttons to white border and white text
    
    added a new textless background

--- src/com/example/beatxbeat/TranscribePageActivity.java
+++ src/com/example/beatxbeat/TranscribePageActivity.java
@@ -1,16 +1,27 @@
 package com.example.beatxbeat;
 
 import java.io.File;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.Bitmap.Config;
+import android.graphics.Canvas;
+import android.graphics.Picture;
+import android.graphics.drawable.PictureDrawable;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore.Images;
 import android.util.Log;
 import android.view.Menu;
+import android.view.View;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.Button;
 
 public class TranscribePageActivity extends Activity {
 	
@@ -34,6 +45,15 @@ public class TranscribePageActivity extends Activity {
 		setContentView(R.layout.activity_transcribe_page);
 		Bundle extras = this.getIntent().getExtras();
 		
+		Button share = (Button) findViewById(R.id.share);
+		
+		share.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View arg0) {
+				shareTranscript((WebView)findViewById(R.id.webView1));
+			}
+		});
 		
 		try {
 			project = new ProjectFile(this, new File(extras.getString(ProjectPageActivity.PROJECT_PATH)), null);
@@ -123,4 +143,33 @@ public class TranscribePageActivity extends Activity {
 
 	}
 
+	
+	/**
+	 * Starts the share intent to share webview as Bitmap
+	 */
+	@SuppressWarnings("deprecation")
+	private void shareTranscript(WebView webView){
+		Picture picture = webView.capturePicture();
+		Bitmap bitMap = pictureDrawable2Bitmap(picture);
+		String pathofBmp = Images.Media.insertImage(getContentResolver(), bitMap,"Transcript", null);
+	    Uri bmpUri = Uri.parse(pathofBmp);
+	    final Intent emailIntent1 = new Intent(     android.content.Intent.ACTION_SEND);
+	    emailIntent1.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+	    emailIntent1.putExtra(Intent.EXTRA_STREAM, bmpUri);
+	    emailIntent1.setType("image/png");
+	    startActivity(emailIntent1);
+	}
+	
+	/**
+	 * Converts picture to bitmap
+	 * @param picture
+	 * @return bitmap
+	 */
+	private static Bitmap pictureDrawable2Bitmap(Picture picture) {
+	    PictureDrawable pd = new PictureDrawable(picture);
+	    Bitmap bitmap = Bitmap.createBitmap(pd.getIntrinsicWidth(), pd.getIntrinsicHeight(), Config.ARGB_8888);
+	    Canvas canvas = new Canvas(bitmap);
+	    canvas.drawPicture(pd.getPicture());
+	    return bitmap;
+	}
 }
